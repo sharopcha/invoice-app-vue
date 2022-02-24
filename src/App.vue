3014655240b6ae679457b3ae80e87e42c9cf7482
sharopcha <napster1202@gmail.com>
2022-02-24 03:31:36 +0200

    Invoice Modal markup

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div v-if="!mobile" class="app flex flex-column">
       <Navigation />
       <div class="app-content flex flex-column">
+        <InvoiceModal />
         <router-view />
       </div>
     </div>
@@ -15,10 +16,12 @@
 
 <script>
   import Navigation from './components/Navigation.vue';
+  import InvoiceModal from './components/InvoiceModal.vue';
 
   export default {
     components: {
       Navigation,
+      InvoiceModal,
     },
     data() {
       return {
