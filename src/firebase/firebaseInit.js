c104a428737e7b64632b8c1ff1ce8fbcd612983c
sharopcha <napster1202@gmail.com>
2022-02-27 01:20:06 +0200

    init firease

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBfCRWpqcaucwS1klM6YfdT9L5yMBHOGdk',
+  authDomain: 'vue-calendar-ec904.firebaseapp.com',
+  databaseURL: 'https://vue-calendar-ec904.firebaseio.com',
+  projectId: 'vue-calendar-ec904',
+  storageBucket: 'vue-calendar-ec904.appspot.com',
+  messagingSenderId: '454209682700',
+  appId: '1:454209682700:web:ed707f4007e81811751d29',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
